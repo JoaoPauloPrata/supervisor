3b9cb7191a4c56afa7af6f33d1357dfae91ed45b
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-08-06 00:57:39 -0300

    fix: constructors brackets and insert endif

--- supervisor/item.cpp
+++ supervisor/item.cpp
@@ -5,9 +5,9 @@
 
 using namespace std;
 
-ItemImpl::ItemImpl() : id(""), name(""), price(0.0);
+ItemImpl::ItemImpl() : id(""), name(""), price(0.0){};
 
-ItemImpl::ItemImpl(string id = "", string name = "", float price = 0.0) : id(id), name(name), price(price);
+ItemImpl::ItemImpl(string id = "", string name = "", float price = 0.0) : id(id), name(name), price(price){};
 
 ItemImpl::~ItemImpl(){};
 
@@ -40,3 +40,5 @@ void ItemImpl::setPrice(float value)
 {
   price = value;
 };
+
+#endif
