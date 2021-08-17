20beee47f1e43fc728d1e345ebe45a12333c7b74
JoaoPauloPrata <joaoppcosta@hotmail.com.br>
2021-08-17 14:13:35 -0300

    Unit Purchase tests

--- test/unit/main.cpp
+++ test/unit/main.cpp
@@ -5,8 +5,10 @@
 #include "./unit_user.h"
 #include "./unit_receipt.h"
 #include "./unit_item.h"
+#include "./unit_purchase.h"
 int main()
 {
+  run_unit_tests_Purchase();
   run_unit_tests_Receipt();
   run_unit_tests_Item();
   run_unit_tests_User();
