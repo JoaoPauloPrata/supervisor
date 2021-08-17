d5dab910b1b697296716fc895e6c76c2fafa13df
JoaoPauloPrata <joaoppcosta@hotmail.com.br>
2021-08-17 12:05:59 -0300

    Removing ambiguity in Item constructor

---
+++ test/unit/unit_item.cpp
@@ -0,0 +1,23 @@
+#include "./unit_item.h"
+
+using namespace std;
+
+void unit_Item_constructor()
+{
+    cout<< "ITEM CONSTRUCTOR TEST STARTING..."<<endl;
+    Item* item = new ItemImpl();
+}
+
+
+
+void run_unit_tests_Item()
+{
+    // unit_Item_constructor();
+    // unit_item_destructor();
+    // unit_Item_getId();
+    // unit_Item_SetId();
+    // unit_Item_getName();
+    // unit_Item_setName();
+    // unit_Item_getPrice();
+    // unit_Item_setPrice();
+}
